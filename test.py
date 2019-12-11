456063aa0b1829d42bd2e954260d38750681ea2e
openmedicalio <54482871+openmedicalio@users.noreply.github.com>
2019-12-11 16:57:04 -0500

    Update test.py

--- test.py
+++ test.py
@@ -9,19 +9,34 @@ def print(x):
 
 langinputs = {
 
-    'en': ['myy nam is peter. i sucks at speelling in english. what is the fuck? '],
+    'en': [
+        "myy nam is peter. i no good at speelling in english. i'm going to say that i think writing take a lot of skill "],
     'es': ['me no puede hablas inglais'],
-    'fr': ['j''aime pas etude maths'],
+    'fr': ['je aimes pas etude maths'],
 
 }
+url = 'https://us-central1-project-318531836785902414.cloudfunctions.net/writing'
 
 s = requests.Session()
 for lang in langinputs:
     for input in langinputs[lang]:
         payload = {
+            'method': 'correct',
             'text': input,
-            'lang':lang,
+            'lang': lang,
         }
-        url = 'https://us-central1-project-318531836785902414.cloudfunctions.net/'
-        r = s.post(url + 'correct', json=payload).json()
+        r = s.post(url, json=payload).json()
         print(r)
+
+prompts = ['I fucked',
+           "you're", ]
+s = requests.Session()
+for prompt in prompts:
+    payload = {
+        'method': 'generate',
+        'text': prompt,
+        'length': 8,
+        'num_samples': 100,
+    }
+    r = s.post(url, json=payload).json()
+    print(r)
